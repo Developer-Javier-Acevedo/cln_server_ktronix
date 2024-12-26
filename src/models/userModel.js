fb66c2d5765ab3c87d10a19ec3ab4e8ea338259c
Javier andres acevedo buitrago <ja.acevedob1@uniandes.edu.co>
2024-12-25 20:59:27 -0500

    structure project add file model user

---
+++ src/models/userModel.js
@@ -0,0 +1,24 @@
+// src/models/userModel.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'user'], default: 'user' },
+}, { timestamps: true });
+
+// Encripta la contraseña antes de guardar
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
